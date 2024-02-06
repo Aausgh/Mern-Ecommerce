f55d1c3100e2290ce1138012b37c78c24927bb77
aayush <aausgh@gmail.com>
2024-02-06 12:27:16 +0545

    category api crud added

--- backend/server.js
+++ backend/server.js
@@ -4,20 +4,27 @@ import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoute.js';
+import categoryRoute from './routes/categoryRoute.js';
 import cors from 'cors';
 
+//configure env
 dotenv.config();
 
+//database config
 connectDB();
 
 const app = express();
 
+//middlewares
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
+//routes
 app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/category', categoryRoute);
 
+//rest api
 app.get("/", (req, res) => {
       res.send("Hello World");
 })
